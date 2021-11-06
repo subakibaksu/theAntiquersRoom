93dbd523920e05fa7ff2b046c87a9e1637538c07
Hayoung <yhao9172@gmail.com>
2021-11-06 23:30:05 +0900

    modify UserService, UserServiceImpl

--- src/main/java/com/theantiquersroom/myapp/service/UserService.java
+++ src/main/java/com/theantiquersroom/myapp/service/UserService.java
@@ -1,7 +1,10 @@
 package com.theantiquersroom.myapp.service;
 
-import com.theantiquersroom.myapp.domain.UserVO;
 import org.springframework.stereotype.Service;
+
+import com.theantiquersroom.myapp.domain.Criteria;
+import com.theantiquersroom.myapp.domain.UserDTO;
+import com.theantiquersroom.myapp.domain.UserVO;
 
 
 @Service
@@ -22,6 +25,28 @@ public interface UserService {
     // 이메일 인증번호 매칭검사
     public abstract boolean confirmEmail(String eCode);
 
+    
+    // 로그인 실행
+    public abstract boolean login(String id, String pwd);
+    
+    // 아이디 찾기 실행
+    public abstract boolean findId(String nickName, String phone);
+    
+    // 비밀번호 재설정
+    public abstract boolean resetPwd(String nickName, String id);
+    
+    // 회원정보 수정
+    public abstract boolean modify(UserDTO userDto);
+    
+    // 회원 탈퇴
+    public abstract boolean remove(String id);
+    
+    // 나의 경매리스트 조회
+    public abstract boolean getMyAuctionList(Criteria cri);
+    
+    // 나의 입찰리스트 조회
+    public abstract boolean getBidList(Criteria cri);
+    
 } // end interface
 
 
--- src/main/java/com/theantiquersroom/myapp/service/UserServiceImpl.java
+++ src/main/java/com/theantiquersroom/myapp/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.theantiquersroom.myapp.service;
 
 
+import com.theantiquersroom.myapp.domain.Criteria;
+import com.theantiquersroom.myapp.domain.UserDTO;
 import com.theantiquersroom.myapp.domain.UserVO;
 import com.theantiquersroom.myapp.mapper.UserMapper;
 import lombok.AllArgsConstructor;
@@ -18,17 +20,6 @@ public class UserServiceImpl implements UserService, InitializingBean, Disposabl
 
     private UserMapper mapper;
 
-    @Override
-    public void destroy() throws Exception {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        // TODO Auto-generated method stub
-
-    }
 
     @Override
     public boolean registerUsers(UserVO user) {
@@ -58,6 +49,62 @@ public class UserServiceImpl implements UserService, InitializingBean, Disposabl
     public boolean confirmEmail(String eCode) {
         // TODO Auto-generated method stub
         return false;
+    }
+
+    
+    @Override
+	public boolean login(String id, String pwd) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean findId(String nickName, String phone) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean resetPwd(String nickName, String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modify(UserDTO userDto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getMyAuctionList(Criteria cri) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getBidList(Criteria cri) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+    
+//---------------------------------------------------//
+    @Override
+    public void destroy() throws Exception {
+    	// TODO Auto-generated method stub
+    	
+    }
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+    	// TODO Auto-generated method stub
+    	
     }
 
 } // end class
