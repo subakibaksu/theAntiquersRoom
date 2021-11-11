3ae98c5696da211d4c32174c46549435b7fb14f5
ggujin <yang97816@gmail.com>
2021-11-11 23:44:26 +0900

    상품등록 DTO, Mapper 추가

--- src/main/java/com/theantiquersroom/myapp/mapper/ProductMapper.java
+++ src/main/java/com/theantiquersroom/myapp/mapper/ProductMapper.java
@@ -1,8 +1,12 @@
 package com.theantiquersroom.myapp.mapper;
 
 
+import com.theantiquersroom.myapp.domain.ProductDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ProductMapper {
+
+    //상품등록
+    public abstract Integer inserProduct(ProductDTO product);
 }
