8588a690e9f006856b4b9ede704e82ffad8b7cfd
DESKTOP-KF37LS7\BB Kim <bb.style.on@gmail.com>
2021-11-16 00:27:28 +0900

    페이징 처리 위한 getPageStart() 추가

--- src/main/java/com/theantiquersroom/myapp/domain/MypageCriteria.java
+++ src/main/java/com/theantiquersroom/myapp/domain/MypageCriteria.java
@@ -15,4 +15,7 @@ public class MypageCriteria {
 	//3. 한 화면당 보여줄 페이지 번호 목록의 길이
 	private Integer pagesPerPage=5;
 	
+	//4. limit 구문에서 시작 부분에 필요한 값을 반환(mybatis에서 사용)
+	public Integer getPageStart() {return (this.currPage -1) * this.amount;}
+	
 } // end class
