ea3c234228f746b74b40411dbbe88c5a404dfb6d
Jinsoo <aaa119o1125@gamil.com>
2021-11-21 19:33:31 +0900

    no message

--- src/main/java/com/theantiquersroom/myapp/mapper/BoardMapper.java
+++ src/main/java/com/theantiquersroom/myapp/mapper/BoardMapper.java
@@ -1,13 +1,4 @@
 package com.theantiquersroom.myapp.mapper;
 
-import java.util.List;
-
-import com.theantiquersroom.myapp.domain.QnADTO;
-
 public interface BoardMapper {
-	
-	public Integer registerQnA(QnADTO dto);
-
-	public List<QnADTO> getQnAList();
-	
 }
--- src/main/java/com/theantiquersroom/myapp/service/BoardService.java
+++ src/main/java/com/theantiquersroom/myapp/service/BoardService.java
@@ -1,13 +1,6 @@
 package com.theantiquersroom.myapp.service;
 
-import java.util.List;
-
-import com.theantiquersroom.myapp.domain.QnADTO;
-
 public interface BoardService {
 
-	public abstract boolean registerQnA(QnADTO dto);
-	
-	public abstract List<QnADTO> getQnAList();
 
 }
