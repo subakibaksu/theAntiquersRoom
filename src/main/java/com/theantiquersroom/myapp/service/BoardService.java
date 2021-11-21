e370667d110fb008482fc394167da411b90a4711
Jinsoo <aaa119o1125@gamil.com>
2021-11-22 00:01:40 +0900

    BoardQnA 다시 올립니다.

--- src/main/java/com/theantiquersroom/myapp/service/BoardService.java
+++ src/main/java/com/theantiquersroom/myapp/service/BoardService.java
@@ -1,6 +1,14 @@
 package com.theantiquersroom.myapp.service;
 
+import java.util.List;
+
+import com.theantiquersroom.myapp.domain.QnADTO;
+
 public interface BoardService {
+
+	public abstract boolean registerQnA(QnADTO dto);
+	
+	public abstract List<QnADTO> getQnAList();
 
 
 }
