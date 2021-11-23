a9ab158eb1bb5e791739cc3a8570706177cc1461
kimdindoo <87438680+kimdindoo@users.noreply.github.com>
2021-11-23 10:30:18 +0900

    Update BoardService.java

--- src/main/java/com/theantiquersroom/myapp/service/BoardService.java
+++ src/main/java/com/theantiquersroom/myapp/service/BoardService.java
@@ -14,9 +14,9 @@ public interface BoardService {
 	public abstract List<QnADTO> getQnAList();
 
 	 /* 문의게시판 목록(페이징 적용) */
-    public List<QnADTO> getQnAListPaging(BoardQnACriteria cri);
-    
-    /* 문의게시판 총 갯수 */
-    public int getQnATotal();
-    
+    	public List<QnADTO> getQnAListPaging(BoardQnACriteria cri);
+
+   	 /* 문의게시판 총 갯수 */
+  	public int getQnATotal();
+
 }
