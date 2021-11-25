c26e401955db4ccdb4f4795a9d71b04515943b18
Kimbeomsoo <rlaqjatn7177@gmail.com>
2021-11-25 16:27:32 +0900

    정렬

--- src/main/java/com/theantiquersroom/myapp/HomeController.java
+++ src/main/java/com/theantiquersroom/myapp/HomeController.java
@@ -1,7 +1,6 @@
 package com.theantiquersroom.myapp;
 
-import java.text.DateFormat;
-import java.util.Date;
+
 import java.util.Locale;
 
 import com.theantiquersroom.myapp.service.ProductService;
@@ -12,15 +11,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
-@Log4j2
 
+@Log4j2
 
 @Controller
 public class HomeController {
@@ -30,19 +25,15 @@ public class HomeController {
     @Setter(onMethod_={@Autowired})
     private ProductService service;
 
-
-    /**
-     * Simply selects the home view to render by returning its name.
-     */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
-        logger.info("Welcome home! The client locale is {}.", locale);
+
         log.debug("main invoked()");
         model.addAttribute("newProductList",service.getNewProduct());
         model.addAttribute("endingProductList",service.getEndingProduct());
         log.debug(service.getNewProduct());
         log.debug(service.getEndingProduct());
         return "main";
-    }
 
+    }
 }
